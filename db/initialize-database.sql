e84751483aabac28174d8d87458a4138baf59629
Sandra Nissan <sandranissan@wlanguest777.eduroam.hj.se>
2022-02-10 18:01:04 +0100

    lagt till relevanta tabeller i databasen

--- db/initialize-database.sql
+++ db/initialize-database.sql
@@ -1,6 +1,23 @@
-CREATE TABLE humans(
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(20)
+CREATE TABLE IF NOT EXISTS adverts(
+    advertId INT AUTO_INCREMENT PRIMARY KEY,
+    advertName VARCHAR(100),
+    advertDescription VARCHAR(100),
+    contact VARCHAR(30)
 );
 
-INSERT INTO humans (name) VALUES ("Alice");
+
+CREATE TABLE IF NOT EXISTS photos(
+    photoId INT AUTO_INCREMENT PRIMARY KEY,
+    nameoFile VARCHAR(100),
+    advert INT,
+    FOREIGN KEY (advert) REFERENCES adverts(advertId),
+    photoDescription VARCHAR(100) 
+);
+
+CREATE TABLE IF NOT EXISTS users(
+    userId INT AUTO_INCREMENT PRIMARY KEY,
+    userName VARCHAR(30),
+    userEmail VARCHAR(50),
+    userPassword VARCHAR(50)
+
+);
